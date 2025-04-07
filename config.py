6a4d53d2542b913e21a0cfff77b7896471158d03
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2025-04-07 10:24:06 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,48 @@
+# Pygame Configuration Module
+
+# Color constants (RGB)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PURPLE = (255, 0 , 255)
+GRAY = (128,128,128)
+# Game window dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Frame rate (frames per second)
+FPS = 60
+
+
+# title
+TITLE ='pygame template'
+
+
+
+#reuse this stuff
+
+#move stuff
+
+#def handle_events (x1,y1):
+   # for event in pygame.event.get():
+       # if event.type == pygame.QUIT:
+            #return x1, y1, False
+        
+   #keys = pygame.key.get_pressed()
+   #if keys[pygame.K_UP]and y1>0:
+   #    y1 -=10
+   # if keys[pygame.K_DOWN]and y1<600:
+   #    y1+=10
+   # if keys[pygame.K_LEFT]and x1>0:
+   #     x1-=10
+   # if keys[pygame.K_RIGHT] and x1<800:
+   #     x1+=10
+#return x1,y1, True
+
+#draw stuff
+
+#def draw_text(screen, text, font, font_color, position, anti_aliased=True, italic=False, bold=False):
+    #img = font.render(text, True, font_color)
+    #screen.blit(img, position,)
